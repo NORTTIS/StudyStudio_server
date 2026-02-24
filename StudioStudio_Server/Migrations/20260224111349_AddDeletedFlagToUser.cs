52f4f70887256dc7c24060b926f1c3e55710b0ee
Vu Xuan Bac <2132k4@gmail.com>
2026-02-24 18:21:09 +0700

    fix build error

---
+++ StudioStudio_Server/Migrations/20260224111349_AddDeletedFlagToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudioStudio_Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeletedFlagToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DeletedFlag",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedFlag",
+                table: "Users");
+        }
+    }
+}
