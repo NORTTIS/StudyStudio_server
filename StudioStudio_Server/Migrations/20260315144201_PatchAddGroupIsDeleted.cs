08fe780304201eafcf2df1c5fecc0edaecf11860
Vu Xuan Bac <2132k4@gmail.com>
2026-03-15 21:48:58 +0700

    fix migration issue

---
+++ StudioStudio_Server/Migrations/20260315144201_PatchAddGroupIsDeleted.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudioStudio_Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class PatchAddGroupIsDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Groups",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Groups");
+        }
+    }
+}
